cb63b305ad824d63bf7bf19d4d14347752205224
Phil Kunz <github@philkunz.com>
2018-11-07 12:44:49 +0100

    fix(core): update

--- dist/elasticlog.plugins.d.ts
+++
@@ -1,4 +0,0 @@
-import * as elasticsearch from 'elasticsearch';
-import * as smartdelay from 'smartdelay';
-import * as smartlogInterfaces from 'smartlog-interfaces';
-export { elasticsearch, smartdelay, smartlogInterfaces };
--- dist/elasticlog.plugins.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const elasticsearch = require("elasticsearch");
-exports.elasticsearch = elasticsearch;
-const smartdelay = require("smartdelay");
-exports.smartdelay = smartdelay;
-const smartlogInterfaces = require("smartlog-interfaces");
-exports.smartlogInterfaces = smartlogInterfaces;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZWxhc3RpY2xvZy5wbHVnaW5zLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vdHMvZWxhc3RpY2xvZy5wbHVnaW5zLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsK0NBQStDO0FBR3RDLHNDQUFhO0FBRnRCLHlDQUF5QztBQUVqQixnQ0FBVTtBQURsQywwREFBMEQ7QUFDdEIsZ0RBQWtCIn0=
